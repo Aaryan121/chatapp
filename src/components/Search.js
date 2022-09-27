1c6343c11ad8881db5fb56769378ef6ea1841b0c
Aaryan121 <aaryan.cs01@gmail.com>
2022-09-27 19:12:05 +0545

    sidebar and chat header completed

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder='find a user'/>
+      </div>
+      <div className="userChat">
+        <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
